5518bcf8ab7fa4f47e28a2fae0c7ac7e5db5b0cd
cuptae <106813678+cuptae@users.noreply.github.com>
2025-04-14 02:47:33 +0900

    Feat: TileMap

--- THProject/Assets/Character.cs
+++ THProject/Assets/Character.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Character : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    Vector2Int curPos;
+    public int moveRange;
+    public GameObject moveRangeTile;
+
+
+    void Awake()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
